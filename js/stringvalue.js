263ba92c4f31d835169b9e1718b13311a354bd4a
Nguyen Cao Hanh <caohanh1502@gmail.com>
2023-06-18 22:57:07 +0700

    Add files via upload

---
+++ js/stringvalue.js
@@ -0,0 +1,5 @@
+const ZALO_PERMISSION_URL = "https://oauth.zaloapp.com/v4/permission"; // Social API
+const ZALO_ACCESS_TOKEN_URL = "https://oauth.zaloapp.com/v4/access_token"; // Social API
+
+const APP_ID = "3703099196777065866"; // FILL your Application ID here!!!
+const APP_AUTH_URL = "https://d6f1-171-232-62-24.ngrok-free.app/auth.html"; // URL to auth.html
