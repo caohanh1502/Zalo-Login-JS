44012f44229931e6275001db121e0290e875c96f
Nguyen Cao Hanh <caohanh1502@gmail.com>
2023-06-18 23:06:20 +0700

    Update stringvalue.js

--- js/stringvalue.js
+++ js/stringvalue.js
@@ -2,4 +2,4 @@ const ZALO_PERMISSION_URL = "https://oauth.zaloapp.com/v4/permission"; // Social
 const ZALO_ACCESS_TOKEN_URL = "https://oauth.zaloapp.com/v4/access_token"; // Social API
 
 const APP_ID = "3703099196777065866"; // FILL your Application ID here!!!
-const APP_AUTH_URL = window.location.href + "/auth.html"; // URL to auth.html
+const APP_AUTH_URL = window.location.href.substring(0, window.location.href.lastIndexOf("/")) + "/auth.html"; // URL to auth.html
